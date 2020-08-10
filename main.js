3072bd166d7ac9f0623e092bea7e0c0083b79b65
Juan Manuel Hernandez Marcano <juanhm93@gmail.com>
2020-08-10 02:21:07 -0400

    logrando agarrar los valores que quiero pero sin poder tener el resultado final

--- main.js
+++ main.js
@@ -33,68 +33,66 @@
 
 import {data} from './array.js';
 let modal = document.getElementById("modal");
+let modals = data
 let selector = document.getElementById("selector");
 let abrir = document.querySelectorAll(".selector")[0];
 let modalS = document.querySelectorAll(".modal-selector")[0];
 
-console.log(data);
-document.addEventListener("DOMContentLoaded", function(event) {
-    const toShow = data.find(el => el.Selection) || null;
-    console.log(toShow);
-       selector.innerHTML +=  toShow.image+
-       '<div class="selector__content--text">'+
-                                ' <p>'+toShow.title+' <span>'+toShow.subtitle+'</span> </p> </div>';
-                                
-       data.forEach(element => {
-           modal.innerHTML +=   ' <div class="selector__content">'+
-                                element.image+
-                                '<div class="selector__content--text">'+
-                                ' <h5>'+element.title+' <span>'+element.subtitle+'</span> </h5>'+
-                                '<p>'+element.description+'</p></div></div>'
-
-                            });
-    
-                        });
-
-                        
-abrir.addEventListener("click",function(e){
-    // console.log(selector.classList.length);
-    
-    if(selector.classList.length <= 1){
-        console.log(selector.classList.length);
-        modalS.style.visibility = "visible";
-        modal.classList.toggle("selector-close");
-        selector.classList.add("prueba");
-    }else{
-        console.log(selector.classList[0]);
-        modal.classList.toggle("selector-close");
-        selector.classList.remove("prueba");
-        setTimeout(function(){
-            modalS.style.visibility = "hidden";
-        },1000)
-
-    }
-    // if(modalS.style.visibility === "hidden"){
-    //     e.preventDefault();
-    //         modalS.style.visibility = "visible";
-    //         modal.classList.toggle("selector-close");
-    // }else{
-    //     modal.classList.toggle("selector-close");
-    // setTimeout(function(){
-    //     modalS.style.visibility = "hidden";
-    // },1000)
+// function seleccionador(variable){
+    //     console.log(variable);
     // }
+    function ahora(){
+        const lista = modal.children;
+        console.log("lista");
     
-});
-        window.addEventListener("click",function(e){
-            console.log(e.target);
-            // e.target = muestra en este caso en consola en codigo la parte que estas tocando con el cursor
-            // if(e.target == modalC){
-            //     modal.classList.toggle("modal-close");
-            //     setTimeout(function(){
-            //         modalC.style.opacity = "0";
-            //         modalC.style.visibility = "hidden";
-            //     },900)
-            // }
+        for(let i=0 ; i < lista.length; i++){  
+            lista[i].addEventListener("click",function(e){
+                let j = lista[i].getAttribute("value");
+                for(let i=0 ; i < lista.length; i++){
+                    if(i == j-1){
+                        data[j-1]['selection'] = true;
+                    } 
+                    data[i]['selection'] = false;
+                }
+            });
+        }
+    }
+    // console.log(data);
+    document.addEventListener("DOMContentLoaded", function(event) {
+        const toShow = data.find(el => el.selection) || null;
+        // console.log(toShow);
+        selector.innerHTML +=  toShow.image+
+        '<div class="selector__content--text">'+
+        ' <p>'+toShow.title+' <span>'+toShow.subtitle+'</span> </p> </div>';
         
+        data.forEach(element => {
+            modal.innerHTML +=   ' <div class="selector__content prueba" value="'+element.id+'">'+
+            element.image+
+            '<div class="selector__content--text">'+
+            ' <h5>'+element.title+' <span>'+element.subtitle+'</span> </h5>'+
+            '<p>'+element.description+'</p></div></div>'
+            
         });
+        ahora()
+        console.log(data);
+    });
+    
+    
+    abrir.addEventListener("click",function(e){
+        
+        if(selector.classList.length <= 1){
+            // console.log(selector.classList.length);
+            modalS.style.visibility = "visible";
+            modal.classList.toggle("selector-close");
+            selector.classList.add("prueba");
+        }else{
+            // console.log(selector.classList[0]);
+            modal.classList.toggle("selector-close");
+            selector.classList.remove("prueba");
+            setTimeout(function(){
+                modalS.style.visibility = "hidden";
+            },1000);
+        }   
+        
+    });
+    
