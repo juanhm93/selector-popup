394bb525fad908c574682162a18a746d4acddb3c
Juan Manuel Hernandez Marcano <juanhm93@gmail.com>
2020-08-10 19:40:00 -0400

    ya funcionando el cambio de selector true

--- main.js
+++ main.js
@@ -1,38 +1,5 @@
-// let cerrar = document.querySelectorAll(".close")[0];
-// let abrir = document.querySelectorAll(".cta")[0];
-// let modal = document.querySelectorAll(".modal")[0];
-// let modalC = document.querySelectorAll(".modal-container")[0];
-
-// abrir.addEventListener("click",function(e){
-//     e.preventDefault();
-//     modalC.style.opacity = "1";
-//     modalC.style.visibility = "visible";
-//     modal.classList.toggle("modal-close");
-// });
-
-// cerrar.addEventListener("click", function(){
-//     modal.classList.toggle("modal-close");
-//     setTimeout(function(){
-//         modalC.style.opacity = "0";
-//         modalC.style.visibility = "hidden";
-//     },900)
-// });
-
-// window.addEventListener("click",function(e){
-//     // console.log(e.target);
-//     // e.target = muestra en este caso en consola en codigo la parte que estas tocando con el cursor
-//     if(e.target == modalC){
-//         modal.classList.toggle("modal-close");
-//         setTimeout(function(){
-//             modalC.style.opacity = "0";
-//             modalC.style.visibility = "hidden";
-//         },900)
-//     }
-
-// });
 
 import {data} from './array.js';
-import { element } from 'prop-types';
 let modal = document.getElementById("modal");
 let selector = document.getElementById("selector");
 let abrir = document.querySelectorAll(".selector")[0];
@@ -40,44 +7,42 @@ let modalS = document.querySelectorAll(".modal-selector")[0];
 
     function ahora(){
         const lista = modal.children;
-        console.log("lista");
-        
+        console.log("lista");   
             for(let i=0 ; i < lista.length; i++){  
                 lista[i].addEventListener("click",function(e){
                 
-                lista[i].setAttribute("alt",true);
-               console.log("atributo ",lista[i].getAttribute("value"));
-               console.log(lista[i]); 
-            //    recorrido(lista[i].getAttribute("value"));
-               // let j = lista[i].getAttribute("value");
-                // for(let i=0 ; i < lista.length; i++){
-                    //     if(i == j-1){
-                        
-                        
-                        // data[j-1]['selection'] = true;
-                        // console.log(data[j-1]);
-                        //     } 
-                        //     data[i]['selection'] = false;
-                        // }
+                    lista[i].setAttribute("alt",true);
+                    console.log("atributo ",lista[i].getAttribute("value"));
+                    recorrido(lista[i].getAttribute("value"))
+                    deleteModal();
+                    addDataModal();
                     });
+                     
             }
     }
+    function deleteModal(){
+        modal.innerHTML='';
+    }        
+    function addDataModal(){
+        data.forEach(element => {
+            modal.innerHTML +=   ' <div class="selector__content" value="'+element.id+'" alt="'+element.selection+'">'+
+            element.image+
+            '<div class="selector__content--text">'+
+            ' <h5>'+element.title+' <span>'+element.subtitle+'</span> </h5>'+
+            '<p>'+element.description+'</p></div></div>'
             
-            function inicio() {
-                const toShow = data.find(el => el.selection) || null;
-                selector.innerHTML +=  toShow.image+
-                '<div class="selector__content--text">'+
-                ' <p>'+toShow.title+' <span>'+toShow.subtitle+'</span> </p> </div>';
-                
-                data.forEach(element => {
-                    modal.innerHTML +=   ' <div class="selector__content prueba" value="'+element.id+'" alt="'+element.selection+'">'+
-                    element.image+
-                    '<div class="selector__content--text">'+
-                    ' <h5>'+element.title+' <span>'+element.subtitle+'</span> </h5>'+
-                    '<p>'+element.description+'</p></div></div>'
-                    
-        });
-        ahora();
+            });
+    }
+
+
+    function inicio() {
+            const toShow = data.find(el => el.selection) || null;
+            selector.innerHTML +=  toShow.image+
+            '<div class="selector__content--text">'+
+            ' <p>'+toShow.title+' <span>'+toShow.subtitle+'</span> </p> </div>';
+            
+            addDataModal();
+           
     }
 
     function modalFunction(){
@@ -85,7 +50,7 @@ let modalS = document.querySelectorAll(".modal-selector")[0];
         if(selector.classList.length <= 1){
             modalS.style.visibility = "visible";
             modal.classList.toggle("selector-close");
-            selector.classList.add("prueba");
+            selector.classList.add("prueba");      
         }else{
             modal.classList.toggle("selector-close");
             selector.classList.remove("prueba");
@@ -104,7 +69,6 @@ let modalS = document.querySelectorAll(".modal-selector")[0];
     
     function seleccionador(e){
         const lista = modal.children;
-        const hijo = lista.children;
 
         const referencia = document.querySelectorAll("div.modals > div.selector__content > svg");
 
@@ -125,6 +89,16 @@ let modalS = document.querySelectorAll(".modal-selector")[0];
         }
     }
     
-    // window.addEventListener( "click", seleccionador)
+    window.addEventListener( "click", ahora)
 
-  
+  function recorrido(item){
+    console.log("item",item);
+    for(let i=0; i < data.length; i++ ){
+        if(data[i]["id"]  == item){
+            data[i]["selection"] = true; 
+        }else{
+            data[i]["selection"] = false; 
+        }
+    }
+    
+  }
