6a33aa3f4d4c58357bdcffd8795eb53feb8ecd1a
Juan Manuel Hernandez Marcano <juanhm93@gmail.com>
2020-08-09 20:30:15 -0400

    funcionalidad del popup lista, y estilos por ahora bien

--- main.js
+++ main.js
@@ -37,14 +37,14 @@ let selector = document.getElementById("selector");
 let abrir = document.querySelectorAll(".selector")[0];
 let modalS = document.querySelectorAll(".modal-selector")[0];
 
-    console.log(data);
+console.log(data);
 document.addEventListener("DOMContentLoaded", function(event) {
-       const toShow = data.find(el => el.Selection) || null;
-        console.log(toShow);
+    const toShow = data.find(el => el.Selection) || null;
+    console.log(toShow);
        selector.innerHTML +=  toShow.image+
-                                '<div class="selector__content--text">'+
+       '<div class="selector__content--text">'+
                                 ' <p>'+toShow.title+' <span>'+toShow.subtitle+'</span> </p> </div>';
-
+                                
        data.forEach(element => {
            modal.innerHTML +=   ' <div class="selector__content">'+
                                 element.image+
@@ -52,22 +52,49 @@ document.addEventListener("DOMContentLoaded", function(event) {
                                 ' <h5>'+element.title+' <span>'+element.subtitle+'</span> </h5>'+
                                 '<p>'+element.description+'</p></div></div>'
 
-       });
+                            });
     
-  });
+                        });
 
-
+                        
 abrir.addEventListener("click",function(e){
-    console.log(modalS.style.visibility);    
-    if(modalS.style.visibility === "hidden"){
-        e.preventDefault();
-            modalS.style.visibility = "visible";
-            modal.classList.toggle("selector-close");
-    }else{
+    // console.log(selector.classList.length);
+    
+    if(selector.classList.length <= 1){
+        console.log(selector.classList.length);
+        modalS.style.visibility = "visible";
         modal.classList.toggle("selector-close");
-    setTimeout(function(){
-        modalS.style.visibility = "hidden";
-    },1000)
-    }
+        selector.classList.add("prueba");
+    }else{
+        console.log(selector.classList[0]);
+        modal.classList.toggle("selector-close");
+        selector.classList.remove("prueba");
+        setTimeout(function(){
+            modalS.style.visibility = "hidden";
+        },1000)
 
+    }
+    // if(modalS.style.visibility === "hidden"){
+    //     e.preventDefault();
+    //         modalS.style.visibility = "visible";
+    //         modal.classList.toggle("selector-close");
+    // }else{
+    //     modal.classList.toggle("selector-close");
+    // setTimeout(function(){
+    //     modalS.style.visibility = "hidden";
+    // },1000)
+    // }
+    
 });
+        window.addEventListener("click",function(e){
+            console.log(e.target);
+            // e.target = muestra en este caso en consola en codigo la parte que estas tocando con el cursor
+            // if(e.target == modalC){
+            //     modal.classList.toggle("modal-close");
+            //     setTimeout(function(){
+            //         modalC.style.opacity = "0";
+            //         modalC.style.visibility = "hidden";
+            //     },900)
+            // }
+        
+        });
