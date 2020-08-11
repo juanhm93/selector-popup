9db574f553e770f1203a5b0449d8205fdcf82c27
Juan Manuel Hernandez Marcano <juanhm93@gmail.com>
2020-08-11 01:23:07 -0400

    cambiando el showhidden del selector pero no consegui resultados positivos

--- main.js
+++ main.js
@@ -5,107 +5,65 @@ let selector = document.getElementById("selector");
 let abrir = document.querySelectorAll(".selector")[0];
 let modalS = document.querySelectorAll(".modal-selector")[0];
 
-    function ahora(){
-        const lista = modal.children;
-        console.log("lista");   
-            for(let i=0 ; i < lista.length; i++){  
-                lista[i].addEventListener("click",function(e){
-                
-                    lista[i].setAttribute("alt",true);
-                    console.log("atributo ",lista[i].getAttribute("value"));
-                    recorrido(lista[i].getAttribute("value"))
-                    deleteModal();
-                    addDataModal();
-                    deleteSelector();
-                    addDataSelector();
-                    modal.classList.toggle("selector-close");
-                    selector.classList.remove("prueba");
-                    setTimeout(function(){
-                        modalS.style.visibility = "hidden";
-                    },1000);
-                    });
-                     
-            }
-    }
-    function deleteModal(){
-        modal.innerHTML='';
-    }        
-    function addDataModal(){
-        data.forEach(element => {
-            modal.innerHTML +=   ' <div class="selector__content" value="'+element.id+'" alt="'+element.selection+'">'+
-            element.image+
-            '<div class="selector__content--text">'+
-            ' <h5>'+element.title+' <span>'+element.subtitle+'</span> </h5>'+
-            '<p>'+element.description+'</p></div></div>'
-            
-            });
-    }
-    function deleteSelector(){
+function addDataModal(){
+    modal.innerHTML='';
+    data.forEach(element => {
+        modal.innerHTML +=   ' <div class="selector__content" value="'+element.id+'" alt="'+element.selection+'">'+
+        element.image+
+        '<div class="selector__content--text">'+
+        ' <h5>'+element.title+' <span>'+element.subtitle+'</span> </h5>'+
+        '<p>'+element.description+'</p></div></div>'
+        
+        });
+}
+function addDataSelector(){
         selector.innerHTML=''
-    }
-    function addDataSelector(){
-            const toShow = data.find(el => el.selection) || null;
-            selector.innerHTML +=  toShow.image+
-            '<div class="selector__content--text">'+
-            ' <p>'+toShow.title+' <span>'+toShow.subtitle+'</span> </p> </div>';
+        const toShow = data.find(el => el.selection) || null;
+        selector.innerHTML +=  toShow.image+
+        '<div class="selector__content--text">'+
+        ' <p>'+toShow.title+' <span>'+toShow.subtitle+'</span> </p> </div>';
 
-    }
+}
+    
+ 
 
-    function inicio() {
-            addDataSelector();
-            addDataModal();
-           
-    }
-
-    function showHiddenSelector(){
-        
-        if(selector.classList.length <= 1){
-            modalS.style.visibility = "visible";
-            modal.classList.toggle("selector-close");
-            selector.classList.add("prueba");  
-            console.log("opcion 1",selector.classList)    
-        }else{
-            modal.classList.toggle("selector-close");
-            selector.classList.remove("prueba");
-            setTimeout(function(){
-                modalS.style.visibility = "hidden";
-            },1000);
-            console.log("opcion 2",selector.classList)    
-        }   
-        
-    }
-    
-    document.addEventListener("DOMContentLoaded", inicio);
-    
-    
-    abrir.addEventListener("click",showHiddenSelector);
-    
-    
-    function seleccionador(e){
-        const lista = modal.children;
-
-        const referencia = document.querySelectorAll("div.modals > div.selector__content > svg");
-
-        console.log(e.target);
-    
+function ahora(){
+    const lista = modal.children;
+    console.log("lista");   
         for(let i=0 ; i < lista.length; i++){  
-            for(let j = 0; j < lista[i].children.length; j++ ){
-                for(let k = 0; k < lista[i].children.length; k++ ){
-                    if(e.target == lista[i] || e.target == lista[i].children || e.target == lista[i].children.item(1).children.item(0)){
-                            console.log(lista[i].children.length); 
-                               console.log(1);
-                               console.log(lista[i].children.item(1).children.item(0));
-                           }
-                }
-
-            }
-
-        }
-    }
+            lista[i].addEventListener("click",function(e){
     
-    window.addEventListener( "click", ahora)
+                console.log(selector.classList); 
+                recorrido(lista[i].getAttribute("value"))
+                addDataModal();
+                showHiddenSelector();
+                addDataSelector();
+                console.log(selector.classList); 
+                });
+                    
+        }
+}        
 
-  function recorrido(item){
+function showHiddenSelector(){
+    
+    console.log("clases ",selector.getAttribute("value")) 
+
+    if(selector.getAttribute("value") == "false"){
+        modalS.style.visibility = "visible";
+        modal.classList.toggle("selector-close");
+        selector.setAttribute("value",true);  
+        console.log("opcion 1",selector)    
+    }else{
+        modal.classList.toggle("selector-close");
+        setTimeout(function(){
+            modalS.style.visibility = "hidden";
+        },1000);
+        selector.setAttribute("value",false);
+        console.log("opcion 2",selector)    
+    }   
+    
+}
+function recorrido(item){
     console.log("item",item);
     for(let i=0; i < data.length; i++ ){
         if(data[i]["id"]  == item){
@@ -115,4 +73,16 @@ let modalS = document.querySelectorAll(".modal-selector")[0];
         }
     }
     
-  }
+}
+
+    
+    // document.addEventListener("DOMContentLoaded", inicio);
+    
+    addDataSelector();
+    addDataModal();
+    
+    abrir.addEventListener("click",showHiddenSelector);
+    window.addEventListener( "click", ahora)
+
+
+  
